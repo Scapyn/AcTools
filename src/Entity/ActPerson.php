d4691b322771866362da956e7061cfe7ca554cdf
Scapyn <mail@yvanrichard.com>
2018-05-29 15:04:41 +0200

    Update Entity ActPerson and migrations - firstname lastname

--- src/Entity/ActPerson.php
+++ src/Entity/ActPerson.php
@@ -37,7 +37,7 @@ class ActPerson
     private $job;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $firstname;
 
